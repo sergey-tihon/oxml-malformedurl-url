7b6a97d049f6572fc7c03fad2dc3c1162d9f1b36
Sergey Tihon <sergey.tihon@gmail.com>
2024-10-03 23:04:52 +0200

    repro code for the issue

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+
+using var fs = File.Open("repro.pptx", FileMode.Open, FileAccess.Read);
+var openSettings = new OpenSettings { AutoSave = false };
+using var doc = PresentationDocument.Open(fs, false, openSettings);
+
+var presentation = doc.PresentationPart!.Presentation!;
+var slideIds = presentation.SlideIdList?.ChildElements;
+Console.WriteLine(slideIds?.Count ?? -1);
